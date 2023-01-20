12fc168e33dc51f2d249b43c35753152947437ba
Anton <>
2023-01-20 09:24:27 +0100

    Add project files.

---
+++ Uppgift 2.3/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hur många meter kan du hoppa i höjdhopp? ");
+            double höjd = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Världsrekordet i höjdhopp är " + (2.45 - höjd) + "meter högre.");
+
+        }
+    }
+}
